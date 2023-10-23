c8d8ab41e69ac582d4734da0b1ce28a5feb3407f
Lilian <92395888+Firzus@users.noreply.github.com>
2023-10-23 17:00:53 +0200

    Ajout de la file d'attente de commandes, de l'allocateur de liste de commandes et de la liste de commandes principale dans la classe Engine. Implémentation réussie de l'initialisation de ces objets dans la méthode InitD3D.

--- Engine/Engine.cpp
+++ Engine/Engine.cpp
@@ -2,28 +2,30 @@
 
 Engine::Engine()
 {
-    if (!InitD3D())
-    {
-        MessageBox(nullptr, L"Failed to initialize Direct3D", L"Error", MB_OK);
-    }
-
+    InitD3D();
     SynchroProcess();
     SetMSAA();
+    CreateCommandAllocator();
+    CreateCommandList();
+    CreateCommandQueue();
 }
 
 Engine::~Engine()
 {
-    
+
 }
 
-bool Engine::InitD3D() {
+void Engine::InitD3D() {
     HRESULT hr = D3D12CreateDevice(
         nullptr,                // pAdapter
         D3D_FEATURE_LEVEL_11_0, // Minimum feature level this app can support
         IID_PPV_ARGS(&mD3DDevice)
     );
 
-    return SUCCEEDED(hr);
+    if (FAILED(hr))
+    {
+        MessageBox(nullptr, L"Failed to initialize Direct3D", L"Error", MB_OK);
+    }
 }
 
 void Engine::SynchroProcess()
@@ -52,6 +54,43 @@ void Engine::SetMSAA()
         MessageBox(0, L"4x MSAA n'est pas supporté sur votre appareil", L"Erreur", MB_OK);
 
         // ex : 1x MSAA ou pas de MSAA
+    }
+}
+
+void Engine::CreateCommandAllocator()
+{
+    HRESULT hr = mD3DDevice->CreateCommandAllocator(D3D12_COMMAND_LIST_TYPE_DIRECT, IID_PPV_ARGS(&mCommandAllocator));
+    if (FAILED(hr))
+    {
+        ThrowIfFailed(hr);
+    }
+}
+
+void Engine::CreateCommandList()
+{
+    HRESULT hr = mD3DDevice->CreateCommandList(
+        0,
+        D3D12_COMMAND_LIST_TYPE_DIRECT,
+        mCommandAllocator.Get(),
+        nullptr,
+        IID_PPV_ARGS(&mCommandList)
+    );
+    if (FAILED(hr))
+    {
+        ThrowIfFailed(hr);
+    }
+}
+
+void Engine::CreateCommandQueue()
+{
+    D3D12_COMMAND_QUEUE_DESC queueDesc = {};
+    queueDesc.Type = D3D12_COMMAND_LIST_TYPE_DIRECT;
+    queueDesc.Flags = D3D12_COMMAND_QUEUE_FLAG_NONE;
+
+    HRESULT hr = mD3DDevice->CreateCommandQueue(&queueDesc, IID_PPV_ARGS(&mCommandQueue));
+    if (FAILED(hr))
+    {
+        ThrowIfFailed(hr);
     }
 }
 
--- Engine/Engine.h
+++ Engine/Engine.h
@@ -6,15 +6,20 @@
 #include <dxgi1_6.h>
 #include <wrl.h>
 
+using namespace Microsoft::WRL;
+
 class Engine
 {
 public:
 	Engine();
 	~Engine();
 
-	bool InitD3D();
+	void InitD3D();
 	void SynchroProcess();
 	void SetMSAA();
+	void CreateCommandQueue();
+	void CreateCommandList();
+	void CreateCommandAllocator();
 
 	void Update();
 
@@ -22,8 +27,11 @@ public:
 	void ThrowIfFailed(HRESULT hr);
 
 private:
-	Microsoft::WRL::ComPtr<ID3D12Device> mD3DDevice;
-	Microsoft::WRL::ComPtr<ID3D12Fence> mFence;
+	ComPtr<ID3D12Device> mD3DDevice;
+	ComPtr<ID3D12Fence> mFence;
+	ComPtr<ID3D12CommandAllocator> mCommandAllocator;
+	ComPtr<ID3D12CommandQueue> mCommandQueue;
+	ComPtr<ID3D12GraphicsCommandList> mCommandList;
 
 	UINT64 mFenceValue = 0;
 
