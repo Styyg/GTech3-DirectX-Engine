161855acb15ae321016c33d894d87326aac67228
Lilian <92395888+Firzus@users.noreply.github.com>
2023-10-23 14:50:34 +0200

    Synchro GPU - CPU
    Étape 2 : Création de l'objet ID3D12Fence et Interrogation des tailles de descripteur
    
    - Ajout d'un pointeur ID3D12Fence pour gérer la synchronisation CPU/GPU.
    - Initialisation de l'objet Fence avec la fonction CreateFence.
    - Récupération des tailles de descripteur pour les types RTV, DSV, et CBV/SRV/UAV via GetDescriptorHandleIncrementSize.
    
    Ce commit met en place les bases nécessaires pour la synchronisation entre le CPU et le GPU et prépare le terrain pour la gestion des descripteurs.

--- Engine/Engine.h
+++ Engine/Engine.h
@@ -1,5 +1,7 @@
 #pragma once
 
+#include <stdexcept>
+#include <string>
 #include <d3d12.h>
 #include <dxgi1_6.h>
 #include <wrl.h>
@@ -10,10 +12,17 @@ public:
 	Engine();
 	~Engine();
 
-	bool InitializeD3D();
+	bool InitD3D();
+	void SynchroProcess();
 
 	void Update();
 
+	// Utils
+	void ThrowIfFailed(HRESULT hr);
+
 private:
 	Microsoft::WRL::ComPtr<ID3D12Device> mD3DDevice;
+	Microsoft::WRL::ComPtr<ID3D12Fence> mFence;
+
+	UINT64 mFenceValue = 0;
 };
